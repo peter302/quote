b2d4c06bfe203f358d861a025309b8e97cefacbc
peter <petermbaik@gmail.com>
2019-11-29 04:22:52 -0500

    generated a Quote-detail component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteDisplayComponent } from './quote-display/quote-display.component';
+import { QuoteDetailComponent } from './quote-detail/quote-detail.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    QuoteDisplayComponent
+    QuoteDisplayComponent,
+    QuoteDetailComponent
   ],
   imports: [
     BrowserModule,
