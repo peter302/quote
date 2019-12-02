5880027b195cd68a91cd58a79175c9946d7c3018
peter <petermbaik@gmail.com>
2019-12-02 15:23:57 +0300

    added a delete icon and its asociated functions

--- src/app/quote-detail/quote-detail.component.ts
+++ src/app/quote-detail/quote-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,Output,EventEmitter} from '@angular/core';
 import{Quote} from '../quote';
 
 @Component({
@@ -8,7 +8,11 @@ import{Quote} from '../quote';
 })
 export class QuoteDetailComponent implements OnInit {
   @Input() quote:Quote
-   
+   @Output() clear=new EventEmitter<boolean>();
+
+   deleteQuote(store:boolean){
+     this.clear.emit(store)
+   }
 
   downvote(){
     this.quote.dislikes=this.quote.dislikes+1
--- src/app/quote-display/quote-display.component.ts
+++ src/app/quote-display/quote-display.component.ts
@@ -8,13 +8,19 @@ import{Quote} from '../quote'
   styleUrls: ['./quote-display.component.css']
 })
 export class QuoteDisplayComponent implements OnInit {
-  quotes:Quote [] =[new Quote("peter kuria","HP has some of the best features",new Date(),"Laptops"),new Quote("joseph njoroge","Nairobi is the best city in the world with the coolest water",new Date(),"our city"),new Quote("johnson john","Isuzu Dmax is far much better than Ford",new Date(),"cars"),new Quote("rispa nancy","the ice is decreasing on the mount",new Date(),"the ice")]
+  quotes:Quote [] =[new Quote("peter kuria","HP has some of the best features",new Date(),"Laptops","moses"),new Quote("joseph njoroge","Nairobi is the best city in the world with the coolest water",new Date(),"our city","wembley"),new Quote("johnson john","Isuzu Dmax is far much better than Ford",new Date(),"cars","Jonte"),new Quote("rispa nancy","the ice is decreasing on the mount",new Date(),"the ice","kambo")]
 
   quotePlus(quote){
     let quoteLength = this.quotes.length;
     quote.id = quoteLength+1;
     quote.completeDate = new Date(quote.completeDate)
     this.quotes.push(quote)
+  }
+
+  quoteDeleted(clear,index){
+    if(clear){
+      this.quotes.splice(index,1)
+    }
   }
 
 
